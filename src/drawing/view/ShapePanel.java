89a0628fdd4ecaafe659f6972d03952bbabadbac
Joseph Al-Abudi <jala9132@740s-pl-120.local>
2017-04-25 13:54:19 -0600

    Added to project

--- src/drawing/view/ShapePanel.java
+++ src/drawing/view/ShapePanel.java
@@ -1,23 +1,38 @@
 package drawing.view;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import drawing.controller.DrawingController;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.time.LocalDateTime;
 
 public class ShapePanel 
 {	
 	private DrawingController baseController;
 	private SpringLayout baseLayout;
-	private ArrayList<Rectangle> rectangleList;
+	private ArrayList<Shape> rectangleList;
+	private ArrayList<Shape> triangleList;
+	private ArrayList<Shape> circleList;
+	private ArrayList<Shape> ellipseList;
+	private ArrayList<Shape> polygonList;
+	private ArrayList<ArrayList<Shape>> shapes;
 	
 	
 	public ShapePanel(DrawingController baseController)
 	{
 		super();
 		this.baseController = baseController;
-		rectangleList = new ArrayList<Rectangle>();
+		rectangleList = new ArrayList<Shape>();
+		triangleList = new ArrayList<Shape>();
+		circleList = new ArrayList<Shape>();
+		ellipseList = new ArrayList<Shape>();
+		polygonList = new ArrayList<Shape>();
+	
 		
 		setupPanel();
 	}
@@ -58,23 +73,105 @@ public class ShapePanel
 		this.setBackground(Color.CYAN);
 		
 		Graphics2D drawingGraphics = (Graphics2D) graphics;
-		for(Rectangle currentRectangle : rectangleList)
+		for(ArrayList<Shape> currentList : shapes)
 		{
-			drawingGraphics.setColor(getRandomColor());
+			drawShapes(currentList, drawingGraphics);
+		}
+	}
+	
+	private void drawShapes(ArrayList<Shape> shapeList, Graphics2D graphics)
+	{
+		for(Shape currentShape : shapeList)
+		{
+			
+			graphics.setColor(getRandomColor());
 			int strokeWidth = (int) (Math.random() * 10) + 1;
-			drawingGraphics.setStroke(new BasicStroke(strokeWidth));
+			graphics.setStroke(new BasicStroke(strokeWidth));
 			
 			int randomness = (int) (Math.random() * 35);
 			
 			if(randomness % 5 == 0 || randomness % 7 == 0)
 			{
-				drawingGraphics.fill(currentRectangle);
+				graphics.fill(currentShape);
+				graphics.setColor(getRandomColor());
+				graphics.draw(currentShape);
 			}
 			else
 			{
-				drawingGraphics.draw(currentRectangle);
+				graphics.draw(currentShape);
 			}
 		}
 	}
 	
+	private void addCircles()
+	{
+		if(circleList.size() > 500)
+		{
+			circleList.clear();
+		}
+		for(int index = 0; index < 30; index++)
+		{
+			int radius = (int) (Math.random() * 25) + 2;
+			int xCorner = (int) (Math.random() * this.getWidth() - 15);
+			int yCorner = (int) (Math.random() * this.getHeight() - 15);
+			Ellipse2D.Double current = new Ellipse2D.Double(xCorner, yCorner, radius, radius);
+			circleList.add(current);
+		}
+		this.repaint();
+	}
+	
+	public void addTriangles()
+	{
+		if(triangleList.size() > 500)
+		{
+			triangleList.clear();
+		}
+		for(int index = 0; index < 30; index++)
+		{
+			int vertexCount = 3;
+			int[] xVertices = new int[vertexCount];
+			int[] yVertices = new int[vertexCount];
+			for(int vertex = 0; vertex < vertexCount; vertex++)
+			{
+				int xCorner = (int) (Math.random() * this.getWidth());
+				int yCorner = (int) (Math.random() * this.getHeight());
+				xVertices[vertex] = xCorner;
+				yVertices[vertex] = yCorner;
+			}
+			Polygon current = new Polygon(xVertices, yVertices, vertexCount);
+			polygonList.add(current);
+		}
+		this.repaint();
+	}
+	
+	public void reset()
+	{
+		for(int index = 0; index < shapes.size(); index++)
+		{
+			shapes.get(index).clear();
+		}
+		this.setBackground(getRandomColor());
+		this.repaint();
+	}
+	
+	public void savePanel()
+	{
+		BufferedImage panelImage = new BufferedImage(this.getSize().width, this.getSize().height, BufferedImage.TYPE_INT_ARGB);
+		Graphics fileGraphics = panelImage.getGraphics();
+		
+		Color background = new Color(getBackground().getRed(), getBackground().getGreen(), getBackground().getBlue());
+		fileGraphics.setColor(background);
+		fileGraphics.fillRect(0, 0, this.getSize().width, this.getSize().height);
+		this.printAll(fileGraphics);
+		fileGraphics.dispose();
+		
+		try
+		{
+			ImageIO.write(panelImage, "png", new File("My Modern art " + LocalDateTime.now().getHour() + "-" + LocalDateTime.now().getMinute() + ".png"));
+		}
+		catch(IOException error)
+		{
+			JOptionPane.showMessageDialog(this, "Save failed");
+		}
+	}
 }
